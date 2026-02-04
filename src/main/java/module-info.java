34c5d2b7ac097e85f7308920dd9c17840d5e1c9b
beaperezm <bpml2001@hotmail.com>
2026-02-04 18:18:44 +0100

    feat: subiendo estructura proyecto front

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.proyectodam.fichappclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.proyectodam.fichappclient to javafx.fxml;
+    exports com.proyectodam.fichappclient;
+    exports com.proyectodam.fichappclient.controller;
+    opens com.proyectodam.fichappclient.controller to javafx.fxml;
+}
